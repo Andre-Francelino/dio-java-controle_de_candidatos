7f3c37841126f9dcd8643150e6c7f7808e2e6157
Andre Francelino <andrefgomes.github.version@gmail.com>
2024-04-04 06:58:30 -0300

    Selecionando Candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,15 +1,37 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 	
 	public static void main(String[] args) {
+		selecaoCandidatos();
+	}
+	
+	static void selecaoCandidatos() {
 		
-		System.out.println("Processo Seletivo");
+		String [] candidatos = {"JAMES", "JULIA", "KAREN", "ROBERTA",  "PAULO", "AUGUSTO", "MONICA", "FRANK", "MIRELLY", "DANIELA"};
 		
-		analisarCandidato(1900.0);
-		analisarCandidato(2200.0);
-		analisarCandidato(2000.0);
+		int candidatosSelecionados = 0;
+		int candidatosAtual = 0;
+		double salarioBase = 2000.0;
 		
+		while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+			String candidato = candidatos[candidatosAtual];
+			double salarioPretendido = valorPretendido();
+			
+			System.out.println("O candidato " + candidato + " solicitou este valor de salário R$ " + salarioPretendido);
+			
+			if(salarioBase >= salarioPretendido) {
+				System.out.println("O candidato " + candidato + " foi selecionado para a vaga!");
+				candidatosSelecionados ++;
+			}
+			candidatosAtual++;
+		}
+	}
+	
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 	
 	static void analisarCandidato(double salarioPretendido) {
